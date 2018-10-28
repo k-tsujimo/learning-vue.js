4de8e5f77332d789eca59a117f33e73ece98c48b
Kenji Tsujimoto <k.tsujimo@gmail.com>
2018-10-28 15:27:29 +0900

    vue data

---
+++ app.js
@@ -0,0 +1,25 @@
+var items = [
+    {
+        name: '鉛筆',
+        price: 300,
+        quantity: 0
+    },
+    {
+        name: 'notebook',
+        price: 400,
+        quantity: 0
+    },
+    {
+        name: 'eraser',
+        price: 500,
+        quantity: 0
+    }
+]
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        items: items
+    }
+})
+window.vm = vm
